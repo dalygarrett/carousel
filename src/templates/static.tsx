f03432d5ab5d8222aadebf380a1c2b2457c424a3
dalygarrett514 <117242181+dalygarrett514@users.noreply.github.com>
2024-06-26 19:06:10 -0400

    Update static.tsx

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -1,5 +1,8 @@
+
+/**
+ * This is an example of how to create a static template that uses getStaticProps to retrieve data.
+ */
 import * as React from "react";
-import { useState, useEffect, useRef } from "react";
 import "../index.css";
 import {
   Template,
@@ -13,20 +16,46 @@ import {
 } from "@yext/pages";
 import Favicon from "../public/yext-favicon.ico";
 
+/**
+ * Not required for static templates, but will contain the stream configuration for
+ * entity-powered templates.
+ */
 export const config: TemplateConfig = {
+  // The name of the feature. If not set the name of this file will be used (without extension).
+  // Use this when you need to override the feature name.
   name: "static-example",
 };
 
+/**
+ * Used to either alter or augment the props passed into the template at render time.
+ * This function will be run during generation and pass in directly as props to the default
+ * exported function.
+ *
+ * This can be used when data needs to be retrieved from an external (non-Knowledge Graph)
+ * source. 
+ *
+ * If the page is truly static this function is not necessary.
+ */
 export const transformProps: TransformProps<TemplateRenderProps> = async (
   data
 ) => {
-  return data;
+  return data
 };
 
+/**
+ * Defines the path that the generated file will live at for production.
+ */
 export const getPath: GetPath<TemplateRenderProps> = () => {
   return `index.html`;
 };
 
+
+/**
+ * This allows the user to define a function which will take in their template
+ * data and produce a HeadConfig object. When the site is generated, the HeadConfig
+ * will be used to generate the inner contents of the HTML document's <head> tag.
+ * This can include the title, meta tags, script tags, etc.
+ */
 export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
   relativePrefixToRoot,
   path,
@@ -43,68 +72,29 @@ export const getHeadConfig: GetHeadConfig<TemplateRenderProps> = ({
           name: "description",
           content: "Static page example meta description.",
         },
-      },
-      {
-        type: "link",
-        attributes: {
-          rel: "stylesheet",
-          href: "https://crossly-previous-buzzard.pgsdemo.com/styles.css",
-        },
-      },
+      }
     ],
   };
 };
 
-const useReviewsWidget = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://crossly-previous-buzzard.pgsdemo.com/reviews-widget.js";
-    script.async = true;
-    
-    script.onload = () => {
-      setIsLoaded(true);
-    };
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isLoaded && window.initWidget) {
-      window.initWidget({
-        baseUrl: 'https://crossly-previous-buzzard.pgsdemo.com/',
-        entityId: '8986600075955733488'
-      });
-    }
-  }, [isLoaded]);
-
-  return isLoaded;
-};
-
+/**
+ * This is the main template. It can have any name as long as it's the default export.
+ * The props passed in here are the direct result from `transformProps`.
+ */
 const Static: Template<TemplateRenderProps> = ({
   relativePrefixToRoot,
   path,
   document,
 }) => {
+
+  // This is the site object from the Knowledge Graph. It contains all the data 
+  // for the site entity, and can be accessed in any template, including static templates. 
   const { _site } = document;
-  const isLoaded = useReviewsWidget();
 
   return (
-    <div>
-      <h1>Reviews Widget Example</h1>
-      <div className="review-carousel">
-        <button className="arrow-button" id="prev-button">&#8592;</button>
-        <div id="review-carousel-container"></div>
-        <button className="arrow-button" id="next-button">&#8594;</button>
-      </div>
-      <div id="average-rating"></div>
-      <div id="star-icons"></div>
-    </div>
+    <>
+    </>
   );
 };
 
